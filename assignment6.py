ff9fab2dec3d09f1d880117ad633880160f8e29b
Vanshika <151389255+vansshika01@users.noreply.github.com>
2024-08-06 09:27:40 +0530

    Update assignment6.py
    added selection sort

--- assignment6.py
+++ assignment6.py
@@ -14,11 +14,35 @@ def bubble_sort(arr):
     print("No of comparisons:%d \nNo. of swaps:%d \n"%(comp,count))
     return arr
 
+def selection_sort(arr):
+    count1=0
+    count2=0
+    for x in range (len(arr)):
+        min=x
+        for y in range (x+1,len(arr)):
+            count1+=1
+            if(arr[y]<arr[x]):
+                count2+=1
+                min=y
+        arr[x],arr[min]=arr[min],arr[x]
+
+    print("No of comparisons:%d \nNo. of change:%d \n"%(count1,count2))
+    return arr
+    
+
+
 
 l=eval(input("Enter the list of numbers:"))
+l2=l.copy()
 print(" ")
 print ("Sorted List:\n")
 start=datetime.now()
 print (bubble_sort(l),"\n")
 end=datetime.now()
 print ("Time: ",end.timestamp()*1000-start.timestamp()*1000," sec")
+print(" ")
+print ("Sorted List:\n")
+start2=datetime.now()
+print (selection_sort(l2),"\n")
+end2=datetime.now()
+print ("Time: ",end2.timestamp()*1000-start2.timestamp()*1000," sec")
