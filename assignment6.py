99215134e786f4c7fb0e2ff09a25a23ff64333a2
Vanshika <151389255+vansshika01@users.noreply.github.com>
2024-08-27 09:34:11 +0530

    Added shell sort

--- assignment6.py
+++ assignment6.py
@@ -11,8 +11,8 @@ def bubble_sort(arr):
                 count+=1
                 swapping=True
         if(not swapping): break
-        print(arr)
-    print("No of comparisons:%d \nNo. of swaps:%d \n"%(comp,count))
+        # print(arr)
+    print("No of comparisons:%d \nNo. of swaps:%d "%(comp,count))
     return arr
 
 def selection_sort(arr):
@@ -27,25 +27,45 @@ def selection_sort(arr):
                 mins=y
         if mins!=x:
             arr[x],arr[mins]=arr[mins],arr[x]
-            print (arr)
+            # print (arr)
 
-    print("No of comparisons:%d \nNo. of change:%d \n"%(count1,count2))
+    print("No of comparisons:%d \nNo. of change:%d "%(count1,count2))
     return arr
+
+def shellSort(arr2, n):
+    g=n//2
+    while g>0:
+        j=g
+        while j<n:
+            i=j-g
+            while i>=0:
+                if arr2[i+g]>arr2[i]:
+                    break
+                else:
+                    arr2[i+g],arr2[i]=arr2[i],arr2[i+g]
+                i=i-g
+            j+=1
+        g=g//2
+    return arr2
     
 
 
-
+n=int(input("Enter the number of elements in list: "))
 l=eval(input("Enter the list of numbers:"))
 l2=l.copy()
+l3=l.copy()
 print(" ")
-print ("Sorted List:\n")
-start=datetime.now()
+print("Bubble Sort: ")
+# start=datetime.now()
 print (bubble_sort(l),"\n")
-end=datetime.now()
-print ("Time: ",end.timestamp()*1000-start.timestamp()*1000," sec")
-print(" ")
-print ("Sorted List:\n")
-start2=datetime.now()
+# end=datetime.now()
+# print ("Time: ",end.timestamp()*1000-sta6rt.timestamp()*1000," sec")
+# print(" ")
+print ("Selection Sort: ")
+# start2=datetime.now()
 print (selection_sort(l2),"\n")
-end2=datetime.now()
-print ("Time: ",end2.timestamp()*1000-start2.timestamp()*1000," sec")
+# # end2=datetime.now()
+# print ("Time: ",end2.timestamp()*1000-start2.timestamp()*1000," sec")
+# print(" ")
+print("Shell Sort:")
+print (shellSort(l3,len(l3)),"\n")
